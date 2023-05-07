a1b3609e95b710051e368e1e740c935aad4a4ead
basil.jung <basil.jung@papilio.ch>
2023-05-07 17:49:04 +0200

    first component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppMainComponent } from './app.main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppMainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppMainComponent } from './app.main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
